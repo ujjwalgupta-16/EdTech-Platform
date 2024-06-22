70e36012fc9288139c5307df42756efa3270a740
Ujjwal <ujjwalgupta1633@gmail.com>
2024-06-22 20:33:02 +0530

    hosting

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://ed-tech-platform-frontend-three.vercel.app",
         credentials: true,
     })
 )
